b45a9aec5d57ac431e8e453edd06b778adf75514
AJFlareau <andrew.flareau@myds.us>
2017-01-06 10:46:41 -0800

    JPG

--- GUI.py
+++ GUI.py
@@ -1,5 +1,6 @@
 from Tkinter import * #gives us access to everything in the Tkinter class
 import tkMessageBox
+from PIL import Image, ImageTk
 
 def buttonpress():
     entrytext = entry1.get()
@@ -9,11 +10,33 @@ def buttonpress():
 
 def addtolist():
     entrytext = entry1.get()
-    listbox1.insert(END, entrytext)
+    if check4dup() == False:
+        listbox1.insert(END, entrytext)
+        findsize()
+    entry1.delete(0,END)
     
 def addtolist2(event):
     entrytext = entry1.get()
-    listbox1.insert(END, entrytext)
+    if check4dup() == False:
+        listbox1.insert(END, entrytext)
+        findsize()
+    entry1.delete(0,END)
+    
+def clearlist(event):
+    listbox1.delete(0, END)
+    findsize()
+    
+    
+def check4dup():
+    names = listbox1.get(0, END)
+    if entry1.get() in names:
+        return True
+    else:
+        return False
+        
+def findsize():
+        label1.config(text=listbox1.size())
+
 
 
 
@@ -21,23 +44,32 @@ def addtolist2(event):
 root = Tk() #gives us a blank canvas object to work with
 root.title("My first GUI program with Tkinter")
 
-button1 = Button(root, text="Button 1", bg="red", command=addtolist)
+button1 = Button(root, text="Enter", bg="red", command=addtolist)
 button1.grid(row=1, column=1)
 
 entry1 = Entry(root)
 entry1.grid(row=1, column=0)
 entry1.bind("<Return>", addtolist2)
 
-label1 = Label(root, text="Dont Push the red Button", bg="blue", anchor=W)
+label1 = Label(root, text="Dont Push the red Button", anchor=W)
 label1.grid(row=0, column=0, sticky=EW, columnspan=2)
 
 listbox1 = Listbox(root)
-listbox1.grid(row=2, column=0, columnspan=2, sticky=EW)
-
+listbox1.grid(row=2, column=0, columnspan=2, sticky=EW, rowspan=10)
+listbox1.bind("<Button-3>", clearlist)
 
 listbox1.insert(END, "Bob")
 listbox1.insert(END, "John")
 listbox1.insert(END, "Erick")
+
+findsize()
+
+image = Image.open("ball.jpg")
+photo = ImageTk.PhotoImage(image)
+
+labe2 = Label(image=photo)
+labe2.image = photo # keep a reference!
+labe2.grid(row=12, column=0)
 
 
 mainloop() #causes the windows to display on the screen until program closes
